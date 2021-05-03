13532a5a5107a490e4fe7cbc930d66efb59636f6
Jfortunati <Jonathan.Fortunati@smartbear.com>
2021-05-03 10:10:03 -0400

    Update publish.js

--- bin/publish.js
+++ bin/publish.js
@@ -5,12 +5,12 @@ const yaml = require("js-yaml");
 const API_KEY = process.env.SWAGGERHUB_KEY;
 
 const openAPIPath = "./docs/openapi.yml";
-const owner = "SmartBearTPM_Org";
-const api = "Book";
+const owner = "SE_Design";
+const api = "CN-Demo-API";
 const version = getVersion();
 
-const PUBLISH_URL = `https://52.16.125.35/apis/v1/${owner}/${api}/${version}/settings/lifecycle?force=false`;
-const MARK_DEFAULT_URL = `https://52.16.125.35/apis/v1/${owner}/${api}/settings/default`;
+const PUBLISH_URL = `https://api.swaggerhub.com/${owner}/${api}/${version}/settings/lifecycle?force=false`;
+const MARK_DEFAULT_URL = `https://api.swaggerhub.com/${owner}/${api}/settings/default`;
 
 function getVersion() {
   const doc = yaml.safeLoad(fs.readFileSync(openAPIPath, "utf8"));
